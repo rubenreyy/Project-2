f83c5e981bbe3638d2b69f1cf894c966dc8189c5
CAN07 <115360508+Cnasty07@users.noreply.github.com>
2025-12-03 17:47:26 -0600

    fixing vulns

--- node_modules/mongoose/types/error.d.ts
+++ node_modules/mongoose/types/error.d.ts
@@ -91,6 +91,9 @@ declare module 'mongoose' {
       errors: { [path: string]: ValidatorError | CastError };
       addError: (path: string, error: ValidatorError | CastError) => void;
 
+      /** Index of the document in insertMany() that failed validation (only set for unordered insertMany) */
+      index?: number;
+
       constructor(instance?: MongooseError);
     }
 
@@ -128,6 +131,13 @@ declare module 'mongoose' {
     export class StrictPopulateError extends MongooseError {
       name: 'StrictPopulateError';
       path: string;
+    }
+
+    export class MongooseBulkSaveIncompleteError extends MongooseError {
+      name: 'MongooseBulkSaveIncompleteError';
+      modelName: string;
+      bulkWriteResult: mongodb.BulkWriteResult;
+      numDocumentsNotUpdated: number;
     }
   }
 }
